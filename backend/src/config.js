4042048d299eb2172b56061e6a9636525af325ce
RockyStarFighter <RockyStarFighter@gmail.com>
2022-04-03 16:55:43 -0500

    Updated Images in slider

--- backend/src/config.js
+++ backend/src/config.js
@@ -130,7 +130,7 @@ const ROYALTY_SHARE = 1000; // Percentage of the token price that goes to the ro
 const ROYALTY_ADDRESS = "0x7aA2c84cfCc077c4bF721a63C2101e543Df62CFb"; // Address that will receive the royalty
 const BASE_URI = null; // only update if you want to manually set the base uri
 const PREREVEAL_TOKEN_URI = null; // only update if you want to manually set the prereveal token uri
-const PRESALE_WHITELISTED_ADDRESSES = []; // only update if you want to manually set the whitelisted addresses
+const PRESALE_WHITELISTED_ADDRESSES = ["0x7aA2c84cfCc077c4bF721a63C2101e543Df62CFb"]; // only update if you want to manually set the whitelisted addresses
 
 // ** OPTIONAL **
 let CONTRACT_ADDRESS = "YOUR CONTRACT ADDRESS"; // If you want to manually include it
