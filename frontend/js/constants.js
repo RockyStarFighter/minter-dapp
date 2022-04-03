bb1711db42d503a9600b500e1aa9cccaee927658
RockyStarFighter <RockyStarFighter@gmail.com>
2022-04-03 15:15:37 -0500

    Added YouTube Channel

--- frontend/js/constants.js
+++ frontend/js/constants.js
@@ -1,9 +1,9 @@
 const contractAddress = "0xf359ef971797a4dd8D48b4F7f416b275cb8A6E82"; //Replace with your own contract address
 const chain = 'rinkeby'; // rinkeby or polygon
 
-const welcome_h1 = "Welcome to the Legend of the Mask Maker NFT Project!!";
+const welcome_h1 = "Welcome to the Legend of the Mask Maker NFT Project!";
 const welcome_h2 = "Connect to MetaMask to Get Started";
-const welcome_p = 'The Legend of the Mask Maker NFT Project is a decentralized utility NFT.  Each NFT owner will get early access to the Alpha release of a video game in development called Legend of the Mask Maker.  The video game will be releaed by the end of 2022. The NFT will also give NFT holders an in game upgrade.  The type of in game upgrade will be determined by the Kitsune Mask color in the purchased NFT.  <a href="https://youtube.com/codestackr"  target="_blank" rel="noopener noreferrer">codeSTACKr</a> YouTube channel.';
+const welcome_p = 'The Legend of the Mask Maker NFT Project is a decentralized utility NFT that gives NFT owners access to the Alpha release of the video game "Legend of the Mask Maker".  The video game is in development and scheduled for Alpha release at the end of 2022. Stay tuned for game dev updates, game play demos, and trailers. <a href="https://www.youtube.com/channel/UCvHgWuKWiy1FBs1Acm9FRkw"  target="_blank" rel="noopener noreferrer">Legend of the Mask Maker Video Game Dev & NFT</a> YouTube channel.';
 const h1_presale_coming_soon = "NFT Drop Coming Soon!!";
 const h1_presale_mint = "Pre-Sale Minting Open!!";
 const h1_public_mint = "Public Minting Open!!";
